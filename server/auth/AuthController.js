fb1787fb6b504a6fdba8a828fea825e85ca6f373
nvora <naimishvora13@gmail.com>
2020-09-23 09:45:05 +0530

    Fixed header sent error and convert method to post

--- server/auth/AuthController.js
+++ server/auth/AuthController.js
@@ -9,7 +9,8 @@ exports.register = function (req, res) {
     let register = authService.Register(req.body, function (err, result) {
         if (err)
             res.send(err);
-        res.send(result);
+        else
+            res.send(result);
     })
 }
 
@@ -22,6 +23,7 @@ exports.login = function (req, res) {
     let login = authService.Login(req.body, function (err, result) {
         if (err)
             res.send(err)
-        res.send(result);
+        else 
+            res.send(result);
     })
 }
